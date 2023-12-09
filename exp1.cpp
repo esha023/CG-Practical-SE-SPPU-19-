e4ef3da96c1c40bb2eaad9faea9bb1af137f1194
esha023 <125105663+esha023@users.noreply.github.com>
2023-12-09 19:44:03 +0530

    Add files via upload

---
+++ exp1.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<graphics.h>
+#include<math.h>
+using namespace std;
+
+class Line
+{
+	int x, y, dx, dy, xi, yi, len;
+	public:
+		void put()
+		{
+			putpixel(x,y,9);
+		}
+		void lin(int x1, int y1, int x2, int y2)
+		{
+			dx = x2 - x1;
+			dy = y2 - y1;
+			
+			if(abs(dx) > abs(dy))
+			{
+				len = abs(dx);
+			}
+			else
+				len = abs(dy);
+				
+			xi = dx/len;
+			yi = dy/len;
+			
+			//initial point
+			x = x1;
+			y = y1;
+			put();
+			
+			//plotting other points
+			for(int i = 1; i<=len; i++)
+			{
+				x = x + xi;
+				y = y + yi;
+				put();
+				delay(5);
+			}
+		}
+};
+
+class Circle
+{
+	public:
+		void cir(int x, int y, int rad)
+		{
+			int x1 = 0;
+			int y1 = rad;
+			putpixel(x1, y1, 6);
+			
+			int pk = 3 - 2*rad;
+			
+			while(x1<=y1)
+			{
+				if(pk<=0)
+				{
+					pk = pk + (4*x1) + 6;
+				}
+				else
+				{
+					pk = pk + (4*(x1-y1)) + 10;
+					y1--;
+				}
+				x1++;
+				
+				putpixel(x+x1, y+y1, 3);
+				putpixel(x-x1, y+y1, 3);
+				putpixel(x+x1, y-y1, 3);
+				putpixel(x-x1, y-y1, 3);
+				putpixel(x+y1, y+x1, 3);
+				putpixel(x-y1, y+x1, 3);
+				putpixel(x+y1, y-x1, 3);
+				putpixel(x-y1, y-x1, 3);
+				delay(10);
+			}	
+		}
+};
+
+int main()
+{
+	Line l;
+	Circle c;
+	int gd = DETECT, gm = DETECT;
+	initgraph(&gd, &gm, NULL);
+	l.lin(100,100,400,100);
+ 	l.lin(100,400,400,400);
+ 	l.lin(100,100,100,400);
+ 	l.lin(400,100,400,400);
+ 	l.lin(250,100,100,250);
+ 	l.lin(100,250,250,400);
+ 	l.lin(250,400,400,250);
+ 	l.lin(250,100,400,250);
+ 	c.cir(250,250,100);
+ 	
+ 	delay(500);
+ 	closegraph();
+ 	
+ 	return 0;
+}
